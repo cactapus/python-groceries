1b8f4422c3ad52439fd028c2b592ed61099c042a
cactapus <satvikliving@gmail.com>
2014-05-09 18:01:11 -0700

    pythonroom update

--- sprite.py
+++ sprite.py
@@ -1,1 +1,18 @@
 # author: cactapus
+import turtle
+
+t = turtle.Turtle()
+
+t.shape("circle")
+
+n = int(input("How many legs does should this sprite have?"))
+
+angle = 360 / n 
+
+for i in range (0, n):
+	
+	t.forward(200)
+	t.stamp()
+	t.backward(200)
+	t.left(angle)
+	
